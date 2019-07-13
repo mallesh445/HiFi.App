66b609b4e8b02618fa63697f25d8b99c11b4189c
malleshkumar06 <malleshkumar06@gmail.com>
2019-07-13 17:04:25 +0530

    Initial Commit

---
+++ HiFi.WebApplication/HiFi.WebApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HiFi.Data.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HiFi.WebApplication.Areas.Identity.IdentityHostingStartup))]
+namespace HiFi.WebApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

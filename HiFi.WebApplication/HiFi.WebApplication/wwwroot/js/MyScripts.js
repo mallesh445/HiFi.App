4a18e775b71895d8b32bd2fca40933b28fc34630
malleshkumar06 <malleshkumar06@gmail.com>
2019-12-07 17:59:20 +0530

    Export to excel bug fixed

--- HiFi.WebApplication/HiFi.WebApplication/wwwroot/js/MyScripts.js
+++ HiFi.WebApplication/HiFi.WebApplication/wwwroot/js/MyScripts.js
@@ -1,38 +1,43 @@
 ﻿
 $(document).ready(function () {
     $("#myFlashMessageDiv").show().delay(2000).fadeOut();
-    var wed = $('#adminCategoriesTable').DataTable({
-        bSort: true,
+    //var wed = $('#adminCategoriesTable').DataTable({
+    //    bSort: true,
 
-        'aoColumns': [
-            { sWidth: "25%", bSearchable: true, bSortable: true, className: "dt-left" },
-            { sWidth: "20%", bSearchable: true, bSortable: true, className: "dt-left" },
-            { sWidth: "10%", bSearchable: true, bSortable: true, className: "dt-left" },
-            { sWidth: "22%", bSearchable: true, bSortable: true, className: "dt-left" },
-            { sWidth: "22%", bSearchable: true, bSortable: true, className: "dt-left" },
-            { sWidth: "10%", bSearchable: true, bSortable: true },
-            { sWidth: "45%", bSearchable: true, bSortable: true }
-            //{ sWidth: "10%", bSearchable: true, bSortable: true },
-            //match the number of columns here for table1
-        ],
-        "bAutoWidth": true,
-        "aLengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]],
-        "info": true,
-        "paging": true,
-        "fnInitComplete": function () {
+    //    'aoColumns': [
+    //        { sWidth: "25%", bSearchable: true, bSortable: true, className: "dt-left" },
+    //        { sWidth: "20%", bSearchable: true, bSortable: true, className: "dt-left" },
+    //        { sWidth: "10%", bSearchable: true, bSortable: true, className: "dt-left" },
+    //        { sWidth: "22%", bSearchable: true, bSortable: true, className: "dt-left" },
+    //        { sWidth: "22%", bSearchable: true, bSortable: true, className: "dt-left" },
+    //        { sWidth: "10%", bSearchable: true, bSortable: true },
+    //        { sWidth: "45%", bSearchable: true, bSortable: true }
+    //        //{ sWidth: "10%", bSearchable: true, bSortable: true },
+    //        //match the number of columns here for table1
+    //    ],
+    //    "bAutoWidth": true,
+    //    "aLengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]],
+    //    "info": true,
+    //    "paging": true,
+    //    "fnInitComplete": function () {
 
-            $("#adminCategoriesTable").css("width", "100%");
-        }
-    });
+    //        $("#adminCategoriesTable").css("width", "100%");
+    //    }
+    //});
 
-    $('#btnExportData').click(function () {
-        $("#adminCategoriesTable").table2excel({
-            // exclude: ".excludeThisClass",
-            //name: "Worksheet Name",
-            filename: "SomeFile.xls" //do not include extension
-            //fnExcelReport();
-        });
-    });
+    //$("#btnExport").click(function () {
+    //    alert("The paragraph was clicked.");
+    //});
+
+    //$('#btnExportData1').click(function () {
+    //    alert("clicked")
+    //    $("#adminCategoriesTable").table2excel({
+    //        // exclude: ".excludeThisClass",
+    //        //name: "Worksheet Name",
+    //        filename: "SomeFile.xls" //do not include extension
+    //        //fnExcelReport();
+    //    });
+    //});
     function fnExcelReport() {
         var tab_text = $('#adminCategoriesTable').val();
 
@@ -48,11 +53,27 @@ $(document).ready(function () {
         bSort: true,
         "bAutoWidth": true,
         "fnInitComplete": function () {
-            $("#adminCategoriesTable").css("width", "100%");
+            $("#adminSubCategoriesTable").css("width", "100%");
         }
     });
 
     $('#adminProductTable').DataTable({
+        bSort: true,
+        "bAutoWidth": true,
+        "fnInitComplete": function () {
+            $("#adminProductTable").css("width", "100%");
+        }
+    });
+
+    $('#adminManufacturesTable').DataTable({
+        bSort: true,
+        "bAutoWidth": true,
+        "fnInitComplete": function () {
+            $("#adminManufacturesTable").css("width", "100%");
+        }
+    });
+
+    $('#adminCategoriesTable').DataTable({
         bSort: true,
         "bAutoWidth": true,
         "fnInitComplete": function () {
